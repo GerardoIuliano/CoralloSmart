4193f6e0d1c010e299b5f0c7fa05edac481913ea
Kersilon <ersiliopet@gmail.com>
2023-01-01 19:47:09 +0100

    create CheckRegistrationResponsabileARPAAction.java, RegistrationResponsabileARPAAction.java
    rispettivamente, action che controlla i campi inseriti per creare un nuovo responsabile ARPA e action che reindirizza al pagina per registrare un responsabile ARPA

---
+++ src/main/java/org/corallosmart/actions/actionsUtente/CheckRegistrationResponsabileARPAAction.java
@@ -0,0 +1,46 @@
+package org.corallosmart.actions.actionsUtente;
+
+import org.corallosmart.actions.actionsUtils.ActionStrategy;
+import org.corallosmart.managers.managersUtente.TableUtenteManager;
+import org.corallosmart.managers.managersUtente.UtenteManager;
+import org.corallosmart.models.modelsUtente.ResponsabileARPA;
+import org.corallosmart.models.modelsUtente.Sostenitore;
+import org.corallosmart.models.modelsUtente.Utente;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
+
+public class CheckRegistrationResponsabileARPAAction implements ActionStrategy {
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) {
+        response.setStatus(HttpServletResponse.SC_ACCEPTED);
+
+        try {
+            String email = request.getParameter("email");
+            String password = request.getParameter("password");
+            String username = request.getParameter("username");
+            String nome = request.getParameter("name");
+            String cognome = request.getParameter("cognome");
+            String codiceFiscale = request.getParameter("codiceFiscale");
+            String telefono = request.getParameter("telefono");
+
+
+            UtenteManager utenteManager = new TableUtenteManager(this.getSource(request));
+            Optional<Utente> optUtente = utenteManager.findUtenteByEmail(email);
+
+            if (optUtente.isPresent()) {
+                request.setAttribute("messaggio", "Email già in uso!");
+                return view("registrationResponsabileARPA");
+            }else{
+                ResponsabileARPA responsabileARPA = new ResponsabileARPA(email, password, username, nome, cognome, codiceFiscale, telefono);
+                utenteManager.createUtente(responsabileARPA);
+                request.setAttribute("messaggio", "Registrazione avvenuta con successo! è possibile effetuare il login");
+                return view("login");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+            return view("500");
+        }
+    }
+}
