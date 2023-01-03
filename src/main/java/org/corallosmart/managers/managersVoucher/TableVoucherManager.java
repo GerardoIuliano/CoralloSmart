ae1145d24f17966744952e777872187a95886c0a
Kersilon <ersiliopet@gmail.com>
2023-01-03 19:39:37 +0100

    update, CheckSostieniciAction.java; TableVoucherManager.java
    Sistemata hashMap;
    piccolissima correzione estetica;

--- src/main/java/org/corallosmart/managers/managersVoucher/TableVoucherManager.java
+++ src/main/java/org/corallosmart/managers/managersVoucher/TableVoucherManager.java
@@ -23,7 +23,7 @@ public class TableVoucherManager extends TableManager implements VoucherManager{
 
     @Override
     public void createVoucher(String descrizione, String importo) throws SQLException {
-        runner.update("INSERT INTO Voucher(descrizione,importo) VALUES(?, ?)", descrizione, importo);
+        runner.update("INSERT INTO Voucher(descrizione, importo) VALUES(?, ?)", descrizione, importo);
     }
 
     @Override
