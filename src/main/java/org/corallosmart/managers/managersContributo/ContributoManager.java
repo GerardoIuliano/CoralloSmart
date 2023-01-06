ddf665508939877bb5375f75820de253ddd0017c
Kersilon <ersiliopet@gmail.com>
2023-01-06 20:07:43 +0100

    update, Contributo.java ContributoManager.java Email.java EmailManager.java
    Aggiunte query per creare/cercare email/contributo senza il campo idVoucher

--- src/main/java/org/corallosmart/managers/managersContributo/ContributoManager.java
+++ src/main/java/org/corallosmart/managers/managersContributo/ContributoManager.java
@@ -9,6 +9,7 @@ import java.util.List;
 public interface ContributoManager {
 
     void createContributo(Contributo contributo) throws SQLException;
+    void createContributoNoVoucher(Contributo contributo) throws SQLException;
     List<Contributo> cercaContributo(int id) throws SQLException;
     public List cercaContributiVoucher(int id) throws SQLException;
     public List<Contributo> listaContributi() throws SQLException;
