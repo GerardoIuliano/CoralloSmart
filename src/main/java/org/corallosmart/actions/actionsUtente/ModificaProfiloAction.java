3d56d6027461926ccf77a8a072885cf50015d24c
Kersilon <ersiliopet@gmail.com>
2023-01-09 13:02:44 +0100

    update, ModificaProfiloAction.java
    ora viene controllato se l'utente è loggato o meno anche nella action

--- src/main/java/org/corallosmart/actions/actionsUtente/ModificaProfiloAction.java
+++ src/main/java/org/corallosmart/actions/actionsUtente/ModificaProfiloAction.java
@@ -1,6 +1,8 @@
 package org.corallosmart.actions.actionsUtente;
 
 import org.corallosmart.actions.actionsUtils.ActionStrategy;
+import org.corallosmart.models.modelsUtente.Sostenitore;
+import org.corallosmart.models.modelsUtente.Utente;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,8 +12,12 @@ public class ModificaProfiloAction implements ActionStrategy {
     public String execute(HttpServletRequest request, HttpServletResponse response) {
         response.setStatus(HttpServletResponse.SC_ACCEPTED);
         try{
-
-            return view("modificaProfilo");
+            Utente utente=(Utente) request.getSession().getAttribute("utente");
+            if(utente != null) {
+                return view("modificaProfilo");
+            }else{
+                return view("login");
+            }
         } catch (Exception e) {
             e.printStackTrace();
             return view("500");
