289f19067acc8ea33cf6454992b487734cd17419
Graziano Giuseffi <grazio1997@gmail.com>
2023-01-13 13:26:49 +0100

    aggiunti commneti javadoc in tutti i managers

--- src/main/java/org/corallosmart/managers/managersRilevamento/TableRilevamentoManager.java
+++ src/main/java/org/corallosmart/managers/managersRilevamento/TableRilevamentoManager.java
@@ -25,6 +25,12 @@ public class TableRilevamentoManager extends TableManager implements Rilevamento
         super(dataSource);
     }
 
+    /**
+     * Query per la ricerca di uno specifico rilevamrnto tramite ID
+     * @param id
+     * @return
+     * @throws SQLException
+     */
     @Override
     public List<Rilevamento> cercaRilevamento(int id) throws SQLException {
         List<Rilevamento> lista = runner.query("SELECT * FROM Rilevamento where id=?",SOS_LIST_MAPPER, id);
@@ -32,17 +38,42 @@ public class TableRilevamentoManager extends TableManager implements Rilevamento
 
     }
 
+    /**
+     * Query per l'inserimento di un nuovo rilevamento all'interno del DB
+     * @param dataRicezione
+     * @param latitudine
+     * @param longitudine
+     * @param DissolvedOxygen
+     * @param OxidationReductionPotential
+     * @param pHTurbidity
+     * @param TotalDissolvedSolids
+     * @param turbidity
+     * @param temperatura
+     * @throws SQLException
+     */
     @Override
     public void inserisciRilevamento(Date dataRicezione,String latitudine, String longitudine, Double DissolvedOxygen, Double OxidationReductionPotential, Double pHTurbidity,Double TotalDissolvedSolids, Double turbidity,String temperatura) throws SQLException {
         runner.update("INSERT INTO Rilevamento(dataRicezione, latitudine, longitudine,DissolvedOxygen,OxidationReductionPotential,pHTurbidity,TotalDissolvedSolids, turbidity, temperatura) VALUES(?,?,?,?,?,?,?,?,?)", dataRicezione, latitudine, longitudine,DissolvedOxygen,OxidationReductionPotential,pHTurbidity,TotalDissolvedSolids, turbidity, temperatura)
     ;}
 
+    /**
+     * Query che permette la ricerca fra un intervallo di date
+     * @param fromDate
+     * @param toDate
+     * @return
+     * @throws SQLException
+     */
     @Override
     public List<Rilevamento> cercaRilevamentoData(Date fromDate, Date toDate) throws SQLException {
         List<Rilevamento> lista= runner.query("SELECT * FROM Rilevamento where dataRicezione>=? AND dataRicezione <=? ORDER BY dataRicezione DESC",SOS_LIST_MAPPER, fromDate, toDate);
                 return lista;
     }
 
+    /**
+     * Query per la ricerca di tutti i rilevamnenti, ordianti per data
+     * @return
+     * @throws SQLException
+     */
     @Override
     public List<Rilevamento> listaRilevamenti() throws SQLException {
         List<Rilevamento> lista= runner.query("SELECT * FROM Rilevamento ORDER BY dataRicezione DESC",SOS_LIST_MAPPER);
